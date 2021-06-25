38d47e2f0d8675174ce256ee23430e2e2b7dfed7
Toianf <72868197+Toianf@users.noreply.github.com>
2021-06-25 17:43:59 +0400

    amelioration

--- GestPlanning/src/main/java/com/mycompany/gestplanning/Dashboard_Form.java
+++ GestPlanning/src/main/java/com/mycompany/gestplanning/Dashboard_Form.java
@@ -7,31 +7,15 @@ import java.awt.Component;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.ListModel;
 import javax.swing.border.Border;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author 1BestCsharp
- */
+
 public class Dashboard_Form extends javax.swing.JFrame {
 
     Connection conn = null;
